506b56634fab110cbb6afe37e1cd29b06bc20390
Thao <nguy3409@umn.edu>
2018-10-31 13:46:39 -0500

    upload .py files

---
+++ input_data.py
@@ -0,0 +1,20 @@
+# Twitter App access keys
+# Fill in your credentials, in quotes (i.e. 'abcxyz')
+CREDENTIALS = {
+'CONSUMER_KEY'    : 'your_consumer_key',
+'CONSUMER_SECRET' : 'your_consumer_secret',
+
+'ACCESS_TOKEN'  : 'your_access_token',
+'ACCESS_SECRET' : 'your_acess_secret'
+}
+
+# Number of tweets to be fetched
+# maximum is 200
+N_TWEETS = 200
+
+# Enter 'photo' if you want to download photos
+# Enter 'video' if you want to fetch video links
+FILE_TYPE = 'photo'
+
+# List of users to fetch tweets
+USERS = ['user_1']
---
+++ twitter_download.py
@@ -0,0 +1,129 @@
+import os, sys
+import tweepy
+import wget
+import pandas as pd
+
+from input_data import *
+
+# API's setup
+def twitter_setup():
+    CONSUMER_KEY    = CREDENTIALS['CONSUMER_KEY']
+    CONSUMER_SECRET = CREDENTIALS['CONSUMER_SECRET']
+
+    ACCESS_TOKEN  = CREDENTIALS['ACCESS_TOKEN']
+    ACCESS_SECRET = CREDENTIALS['ACCESS_SECRET']
+
+    # Authentication and access using keys
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication
+    api = tweepy.API(auth)
+    return api
+
+def get_all_tweets(screen_name, count, extend):
+    '''
+    Get all (maximum of 3240) tweets from an user
+    '''
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+    api = twitter_setup()
+
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name, count=count,
+                                    tweet_mode='extended', include_rts=False, exclude_replies=True)
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    if extend==True:
+        #keep grabbing tweets until there are no tweets left to grab
+        while len(new_tweets) > 0:
+            print("getting tweets before %s" % (oldest))
+
+            #all subsiquent requests use the max_id param to prevent duplicates
+            new_tweets = api.user_timeline(screen_name = screen_name,count=count,
+                                            tweet_mode='extended', include_rts=False,
+                                            exclude_replies=True, max_id=oldest)
+
+            #save most recent tweets
+            alltweets.extend(new_tweets)
+
+            #update the id of the oldest tweet less one
+            oldest = alltweets[-1].id - 1
+
+            print("...%s tweets fetched so far" % (len(alltweets)))
+
+    else:
+        print("...%s tweets fetched!" % (len(alltweets)))
+
+    return alltweets
+
+def get_media(tweets, type):
+    '''
+    Get all urls of user's photos/videos
+    '''
+    photos = set()
+    videos = set()
+    for status in tweets:
+        # check if tweet as extended entitites (with media files)
+        if hasattr(status, 'extended_entities'):
+            media = status.extended_entities.get('media', [])
+            for i in range(len(media)):
+                # check if the media is photo, get photo url
+                if media[i]['type'] == 'photo':
+                    photos.add(media[i]['media_url'])
+                # if media file is video, get .mp4 url
+                elif media[i]['type'] == 'video':
+                    videos.add(media[i]['video_info'].get('variants')[0]['url'])
+        else:
+            continue
+
+    if type == 'photo':
+        return photos
+    elif type == 'video':
+        return videos
+
+def create_folder(output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+def download_images(photo_urls, output_folder):
+    create_folder(output_folder)
+
+    for url in photo_urls:
+        # Only download if picture hasn't been in the folder yet
+        file_name = os.path.split(url)[1]
+        if not os.path.exists(os.path.join(output_folder, file_name)):
+            wget.download(url +":orig", out = output_folder+'/'+file_name)
+
+def get_video_urls(username, video_urls):
+    data = pd.DataFrame(data = [url for url in video_urls], columns=['Video Links'])
+    # write to csv file
+    data.to_csv(username+'_video_urls.csv', index=False)
+
+def main():
+    url_type = FILE_TYPE
+
+    for name in USERS:
+        outdir = name+'_download'
+        print("Fetching tweets for user %s......." % name)
+
+        # set extend to False if you don't want to fetch tweets continuously
+        tweets = get_all_tweets(name, N_TWEETS, extend=True)
+
+        urls = get_media(tweets, type=url_type)
+
+        if url_type == 'photo':
+            download_images(urls, outdir)
+            print("Download complete!")
+        elif url_type == 'video':
+            get_video_urls(name, urls)
+            print("Writing complete!")
+
+if __name__=='__main__':
+    main()
